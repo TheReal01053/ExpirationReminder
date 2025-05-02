24fac3bef48f77d5b20d821a22c8557225a4a409
TheReal01053 <50691110+TheReal01053@users.noreply.github.com>
2025-05-02 14:39:16 +1000

    Update Send.java

--- src/main/java/com/michealt/ExpirationReminder/mail/Send.java
+++ src/main/java/com/michealt/ExpirationReminder/mail/Send.java
@@ -22,41 +22,11 @@ import java.util.Properties;
 public class Send {
 
     public static void SendMail(License contact, int days) throws Exception {
-        /*final String username = "micheal.thompson@claratti.com";
-        final String password = "Ak4K3OL1";
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "outlook.office365.com");
-        props.put("mail.smtp.port", "587");
-
-
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
-        //message instance
-        Message message = new MimeMessage(session);
-        //set who the message is coming from
-        message.setFrom(new InternetAddress("micheal.thompson@claratti.com"));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(contact.getContactEmail()));
-
-        Date date = new Date();
-
-        message.setSubject("License Due to Expire in " + days + " days from " + date.toString().substring(0, 10));
-        message.setDataHandler(new DataHandler(new HTMLDataSource(TestApplication.htmlMessage.replace("null", "").
-                                                                    replace("_CONTACT_", contact.getLicenseContact()).
-                                                                    replace("_SERIAL_", contact.getLicenseSerial()).
-                                                                    replace("_LICENSE_", contact.getLicenseName()).
-                                                                    replace("_CLIENT_", contact.getClientName()).
-                                                                    replace("_DATE_", contact.getDisplayedDate()).replace("_DAYS_", "" + days))));
 
         Transport.send(message);*/
 
-        final String username = "micheal.thompson@claratti.com";
-        final String password = "Cl0uddcc!";
+        final String username = "";
+        final String password = "";
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
